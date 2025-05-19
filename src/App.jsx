0bdce3cd6da3711611d4909171252db306549ca5
𝐌7𝐃-𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 <moll71738@gmail.com>
2025-05-19 19:18:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -478,10 +478,6 @@ const App = () => {
 >
   {darkMode ? (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={darkMode ? '#fff' : '#000'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M21 12.79A9 9 0 1 1 11.21 3 7.958 7.958 0 0 1 21 12.79z"/>
-    </svg>
-  ) : (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={darkMode ? '#fff' : '#000'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
       <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -491,6 +487,10 @@ const App = () => {
       <line x1="21" y1="12" x2="23" y2="12"></line>
       <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
       <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+    </svg>
+  ) : (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={darkMode ? '#fff' : '#000'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7.958 7.958 0 0 1 21 12.79z"/>
     </svg>
   )}
 </button>
